489247d7d460b217c49bcf80c6817ce8953efe90
Harold Arguello <hjarguellor@gmail.com>
2024-05-26 17:22:01 -0600

    cambio de color en el background

--- lib/page/registration_page.dart
+++ lib/page/registration_page.dart
@@ -11,42 +11,52 @@ class RegistrationPage extends StatefulWidget {
 
 class _RegistrationPageState extends State<RegistrationPage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Registro de Usuario'),
-        centerTitle: true,
-        backgroundColor: Colors.amber,
+  Widget build(BuildContext context) =>
+     Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background2.jpg',),
+          fit: BoxFit.cover,)
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12.0),
-        child: Center(
-              child: SizedBox(
-                width:  450,
-                child: Column(
-                  children: [
-                    TextFieldWidget(
-                      label: 'Nombre de Usuario',
-                      placeholder: 'Nombre',
-                      icon: Icons.person_2_rounded,       
-                    ),
-                    TextFieldWidget(
-                      label: 'correo electronico',
-                      placeholder: 'Email',
-                      icon: Icons.mail,       
-                    ),
-                  ],  
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text('Registro de Usuario'),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body:
+          const Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Center(
+                child: SizedBox(
+                  width:  450,
+                  child: Column(
+          
+                    children: [
+                      TextFieldWidget(
+                        label: 'Nombre de Usuario',
+                        placeholder: 'Nombre',
+                        icon: Icons.person_2_rounded,       
+                      ),
+                      TextFieldWidget(
+                        label: 'correo electronico',
+                        placeholder: 'Email',
+                        icon: Icons.mail,       
+                      ),
+                    ],  
+                  ),
+                  
                 ),
-                
-              ),
+            ),
           ),
         ),
-      );
+    );
              
 
       
   }
-}
+
 
 
 class TextFieldWidget extends StatelessWidget{ 
@@ -69,29 +79,33 @@ this.isTextArea= false , this.controller
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(label, style:const TextStyle( color: Colors.green, fontSize: 18)  ),
-        const SizedBox(height: 5.0),
-        Container(
-          padding: const EdgeInsets.only(left: 10.0),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: TextField(
-            controller: controller,
-            maxLines: isTextArea ? 6 : null,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: placeholder,
-              suffixIcon: icon != null ? Icon(icon, color: Colors.amber): null,
+    return Container(
+      color: Colors.white,
 
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(label, style:const TextStyle( color: Colors.green, fontSize: 18)  ),
+          const SizedBox(height: 5.0),
+          Container(
+            padding: const EdgeInsets.only(left: 10.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(10.0),
             ),
-          ),
-        )
-      ],
+            child: TextField(
+              controller: controller,
+              maxLines: isTextArea ? 6 : null,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: placeholder,
+                suffixIcon: icon != null ? Icon(icon, color: Colors.red): null,
+      
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
