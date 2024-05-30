8682272137d4754b60f5908d68e6e9f8f3969489
Maximo <Guzmanmax1998@gmail.com>
2024-05-30 13:47:24 -0600

    Informacion perfil base

--- lib/page/myaccountpage.dart
+++ lib/page/myaccountpage.dart
@@ -1,19 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:mynapopizza/page/home_page.dart';
 
-class MyAccountPage extends StatelessWidget {
-  const MyAccountPage({super.key});
+class MyAccountPage extends StatefulWidget {
+  const MyAccountPage({Key? key}) : super(key: key);
 
-  
+  @override
+  _MyAccountPageState createState() => _MyAccountPageState();
+}
 
+class _MyAccountPageState extends State<MyAccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Text('Mi Cuenta',
-          style: TextStyle(fontWeight: FontWeight.w900,
-          fontSize: 28,),
-        ),
-      )
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
+      body: MyAccountBody(),
+    );
+  }
+}
+
+class MyAccountBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/usuario.png'), // Cambia por tu propia imagen
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Nombre de Usuario',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Correo electrónico: admin@admin.com', // Cambia por tu propia dirección de correo electrónico
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              // Acción al presionar el botón (por ejemplo, cerrar sesión)
+            },
+            child: Text('Cerrar Sesión'),
+          ),
+          ElevatedButton(
+            onPressed:(){
+              Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) =>const HomePage()),
+      );
+          },
+          child: Text('Volver'),)
+        ],
+      ),
     );
   }
 }
