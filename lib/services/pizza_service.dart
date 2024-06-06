781b8402ddc8ac3a2e67da3d7d28df963543f1a8
Maximo <Guzmanmax1998@gmail.com>
2024-06-05 18:04:29 -0600

    cambios funcion registro pizza

--- lib/services/pizza_service.dart
+++ lib/services/pizza_service.dart
@@ -6,14 +6,10 @@ final FirebaseFirestore _db = FirebaseFirestore.instance;
 
 Future<bool> agregarUsuario(Pizza pizza) async {
   try {
-    // Intenta agregar el usuario a la colección de Firestore
     await _db.collection('pizzas').add(pizza.toMap());
-    // Si se completa correctamente, devuelve true
     return true;
   } catch (e) {
-    // Si ocurre un error, imprime el mensaje de error
-    print('Error al agregar usuario: $e');
-    // Devuelve false para indicar que no se pudo agregar el usuario
+    print('Error al registrar la pizza: $e');
     return false;
   }
 }
