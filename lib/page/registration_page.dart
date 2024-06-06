049e78869ed510e6fada60fc743a3ad08e5ad1b2
Maximo <Guzmanmax1998@gmail.com>
2024-06-05 18:28:40 -0600

    correpcio articuloscarrito con el modelo pizza

--- lib/page/registration_page.dart
+++ lib/page/registration_page.dart
@@ -188,6 +188,7 @@ void registrarUsuario() async {
                         
                         }*/
                          registrarUsuario();
+                         
                         },
                       ),
                     ),
